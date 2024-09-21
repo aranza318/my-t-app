7715284aaacb18ab37bad9bb9cfd8a8825a36375
aranza318 <akadikinesiologia@hotmail.com>
2024-09-20 21:03:10 -0300

    ok app

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola Coder!</Text>
+      <Text>Primera app de Aránzazu Galvaliz</Text>
       <StatusBar style="auto" />
     </View>
   );
